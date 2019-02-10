16a233d5ed0add31bea9a66dfd344a40325f8276
DeepLearning VM <google-dl-platform@googlegroups.com>
2019-02-10 15:37:28 +0000

    fix plot

--- plot.py
+++ plot.py
@@ -7,9 +7,9 @@ fig, ax = plt.subplots(2, 1, figsize=(15, 15))
 for net in ('ResNet20', 'ResNet32', 'ResNet44', 'ResNet56'):
     checkpoint = torch.load('pretrained/' + net + '.pth')
     history = checkpoint['history']
-    c = ax[0].plot(history['acc'], linewidth=1, label=net)
+    c = ax[0].plot(history['acc'], linewidth=0.5, label=net)
     ax[0].plot(history['val_acc'], linewidth=0.5, color=c[0].get_color())
-    c = ax[1].plot(history['loss'], linewidth=1, label=net)
+    c = ax[1].plot(history['loss'], linewidth=0.5, label=net)
     ax[1].plot(history['val_loss'], linewidth=0.5, color=c[0].get_color())
 
 ax[0].plot(history['val_acc'])
