b7e229889d358f7d816fddcbb1432dda10caba34
DeepLearning VM <google-dl-platform@googlegroups.com>
2019-02-10 15:31:24 +0000

    add plot

--- plot.py
+++ plot.py
@@ -2,30 +2,27 @@ import torch
 import matplotlib.pyplot as plt
 import numpy as np
 
-checkpoint = torch.load('resnet20.pth')
+fig, ax = plt.subplots(2, 1, figsize=(15, 15))
 
-def plot(history):
-    fig, ax = plt.subplots(2, 1, figsize = (10, 10))
+for net in ('ResNet20', 'ResNet32', 'ResNet44', 'ResNet56'):
+    checkpoint = torch.load('pretrained/' + net + '.pth')
+    history = checkpoint['history']
+    c = ax[0].plot(history['acc'], linewidth=1, label=net)
+    ax[0].plot(history['val_acc'], linewidth=0.5, color=c[0].get_color())
+    c = ax[1].plot(history['loss'], linewidth=1, label=net)
+    ax[1].plot(history['val_loss'], linewidth=0.5, color=c[0].get_color())
 
-    ax[0].plot(history['acc'])
-    ax[0].plot(history['val_acc'])
-    ax[0].set_title('model accuracy')
-    ax[0].set_ylabel('accuracy')
-    ax[0].set_xlabel('epoch')
-    ax[0].legend(['train', 'validation'])
-    ax[0].grid(b=True, linestyle='--')
+ax[0].plot(history['val_acc'])
+ax[0].set_title('model accuracy')
+ax[0].set_ylabel('accuracy')
+ax[0].set_xlabel('epoch')
+ax[0].legend()
+ax[0].grid(b=True, linestyle='--')
+
+ax[1].set_title('model loss')
+ax[1].set_ylabel('loss')
+ax[1].set_xlabel('epoch')
+ax[1].legend()
+ax[1].grid(b=True, linestyle='--')
     
-    ax[1].plot(history['loss'])
-    ax[1].plot(history['val_loss'])
-    ax[1].set_title('model loss')
-    ax[1].set_ylabel('loss')
-    ax[1].set_xlabel('epoch')
-    ax[1].legend(['train', 'validation'])
-    ax[1].grid(b=True, linestyle='--')
-    
-    fig.subtitle('Resnet')
-
-    plt.savefig('plot.png', dpi=100)
-    plt.show()
-
-plot(checkpoint['history'])
+plt.savefig('plot.png', dpi=100)
